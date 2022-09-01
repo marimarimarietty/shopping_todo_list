58d25eb3f78ab75db514dc5b2acd64a6a622f81d
marimarimarietty <cnblue.yonghwa.maroon5@gmail.com>
2022-09-01 16:57:33 +0900

    feat: modify app

--- app.py
+++ app.py
@@ -47,9 +47,9 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-		line_bot_api.reply_message(
-			event.reply_token,
-			TextSendMessage(text=event.message.text))
+  line_bot_api.reply_message(
+    event.reply_token,
+    TextSendMessage(text=event.message.text))
 
 # @handler.add(MessageEvent, message=TextMessage)
 # def handle_message(event):
